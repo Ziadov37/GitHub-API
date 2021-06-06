352f5adae57354c06d63a495c51afb84192f1841
Ziadov37 <77000963+Ziadov37@users.noreply.github.com>
2021-06-06 16:04:14 +0100

    Some Modifications

--- axios.js
+++ axios.js
@@ -1,14 +1,25 @@
-const userName = document.getElementById('userName')
-const serach = document.getElementById('search')
+const search = document.getElementById('search')
+const submit = document.getElementById('submit')
 const dataName = document.getElementById('name')
+const repos = document.getElementById('repos_url')
+// const avatar = document.getElementById('avatar_url')
+const image = document.getElementById('avatar_url').innerHTML=`<img src="">`
 var namee ;
 
-search.addEventListener('click',async(e)=>{
+submit.addEventListener('click',async(e)=>{
     e.preventDefault();
     try {
-        const res = await axios.get(`https://api.github.com/users/${userName.value}`)
+        const res = await axios.get(`https://api.github.com/users/${search.value}`)
         namee = res.data.name
         dataName.innerHTML = res.data.name
+
+        const reposs = await axios.get(`https://api.github.com/users/${search.value}`)
+        repos.innerHTML= res.data.repos_url
+
+        const avatarr = await axios.get(`https://api.github.com/users/${search.value}`)
+        Image.innerHTML= res.data.avatar_url
+        Image.setAttribute('src',res.data.avatar_url)
+        console.log(avatarr)
     } catch (error) {
         
     }
