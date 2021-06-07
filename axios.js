7fa9cc25815009aef4e41ea4f4ed865e814af326
Ziadov37 <77000963+Ziadov37@users.noreply.github.com>
2021-06-07 14:02:49 +0100

    Adding repos to the file

--- axios.js
+++ axios.js
@@ -2,8 +2,10 @@ const search = document.getElementById('search')
 const submit = document.getElementById('submit')
 const dataName = document.getElementById('name')
 const repos = document.getElementById('repos_url')
+const repo = document.getElementById('repo')
+
 // const avatar = document.getElementById('avatar_url')
-const image = document.getElementById('avatar_url').innerHTML=`<img src="">`
+// const image = document.getElementById('avatar_url').innerHTML=`<img src="">`
 var namee ;
 
 submit.addEventListener('click',async(e)=>{
@@ -16,10 +18,39 @@ submit.addEventListener('click',async(e)=>{
         const reposs = await axios.get(`https://api.github.com/users/${search.value}`)
         repos.innerHTML= res.data.repos_url
 
-        const avatarr = await axios.get(`https://api.github.com/users/${search.value}`)
-        Image.innerHTML= res.data.avatar_url
-        Image.setAttribute('src',res.data.avatar_url)
-        console.log(avatarr)
+        // const avatarr = await axios.get(`https://api.github.com/users/${search.value}`)
+        // Image.innerHTML= res.data.avatar_url
+        // Image.setAttribute('src',res.data.avatar_url)
+let count = 0;
+        const arepo = await axios.get(`https://api.github.com/users/${search.value}/repos`)
+        repo.innerHTML= arepo;
+        
+
+
+
+        
+        for (const key in arepo) {
+           
+                
+          
+
+          
+            
+           var data =  `${arepo.data[count].name}  `;
+           var p = document.createElement('p');
+            repo.appendChild(p).innerHTML = data
+
+                
+                count++
+                
+
+
+                
+            
+        }
+        
+
+
     } catch (error) {
         
     }
